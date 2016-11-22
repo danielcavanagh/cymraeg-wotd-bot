ef84426c5be844a99c2c33d33adcf1e665ee07f6
Daniel Cavanagh <danielcavanagh85@gmail.com>
2016-11-22 12:04:41 +1000

    move classes into own files

---
+++ gpc.rb
@@ -0,0 +1,106 @@
+require 'nokogiri'
+require 'open-uri'
+require './bangor-parser.rb'
+require './gpc-parser.rb'
+
+class CymraegBot
+  class GPCParser
+    attr_reader :doc
+
+    def initialize(word)
+      @doc =
+        if word.is_a? String or word.is_a? Integer
+          word
+        else word
+        end
+    end
+
+    def parse
+      if doc.search('.p12c').any? {|elem| elem.text =~ /\b(19|2\d)\d\d\b|2\d(-\d)?g./ }
+        return {
+          word: word,
+          pronunciations: Pronouncer.new(word).pronunciations,
+          plurals: plurals,
+          stem: stem,
+          types: types,
+          alt: alt,
+          meanings: meanings,
+        }
+      else return nil
+      end
+    end
+
+    def alt
+      @alt ||= welsh_lines[2]&.text =~ /\(b\./ ? feminine_alt : nil
+    end
+
+    def types
+      @types ||= (welsh_lines[2].css('h').map do |type|
+        case type.text
+        when 'a.' then 'adjective'
+        when 'eg.', 'rhif.' then 'noun (masculine)'
+        when 'eb.' then 'noun (feminine)'
+        when 'eg.b.' then 'noun (masculine/feminine)'
+        when 'e.ll.' then 'noun (plural)'
+        when 'ba.', 'bg.', 'bg.a.' then 'verbnoun'
+        when 'cys.' then 'conjunction'
+        else nil
+        end
+      end.compact rescue [])
+    end
+
+    def feminine_alt
+      @feminine_alt ||= welsh_lines.length >= 3 ? welsh_lines[2].text.match(/\(b. ([[:word:]]+)/)[1] + ' (feminine)' : nil
+    end
+
+    def plurals
+      return @plurals if @plurals
+      return @plurals = [] if is_number? or welsh_lines.length < 3
+
+      @plurals =
+        welsh_lines[2].text.scan(/ll.(?: (?:\(prin\) )?[[[:word:]]()-]+,?)+/).map do |entry|
+          entry.sub('ll. ', '').split(', ')
+        end.flatten.map do |pattern|
+          if pattern.include?('(prin) ') or pattern.include?('hefyd fel') or pattern.include?('weithiau fel') then nil
+          elsif pattern[0] == '-' then headword + pattern[1..-1]
+          else pattern
+          end
+        end.compact
+    end
+
+    def stem
+      if is_verb? then headword.sub(/af$/, '') + '-'
+      else nil
+      end
+    end
+
+    def headword
+      @headword ||= doc.css('head').first.text.sub(/\d+$/, '')
+    end
+
+    def word
+      @word ||=
+        if is_verb? && welsh_lines[0].text.include?(':') then welsh_lines[0].text.match(/:\s+([[[:word:]]()-]+)/)[1]
+        else headword
+        end
+    end
+
+    def meanings
+      @meanings ||= doc.css('.p22').map {|m| m.text.sub(/(?<!also fig)\.$/, '') }
+    end
+
+    def is_number?
+      @is_number ||= welsh_lines[2]&.text.include?('rhif.')
+    end
+
+    def is_verb?
+      @is_verb ||= types.any? && types.first.include?('verb')
+    end
+
+    private
+
+    def welsh_lines
+      @welsh_lines ||= doc.css('.p12')
+    end
+  end
+end
---
+++ pronouncer.rb
@@ -0,0 +1,174 @@
+# encoding: utf-8
+
+module CymraegBot
+  module Refinements
+    refine Array do
+      def scan(initial)
+        inject([initial]) {|acc, el| acc << yield(acc.last, el) }.drop(1)
+      end
+    end
+  end
+
+  class Pronouncer
+    using Refinements
+
+    UncoveredConsonant = 'tʃ|dʒ|[bdðfghjklɫɬmm̥nn̥prr̥sʃtθvx]'
+    Consonant = "(#{UncoveredConsonant})"
+    Vowel = '[ɑa@eɛ%iɪ!ɨɨ̞#oɔ&uʊə]'
+    LongableVowel = '[@%!#&]'
+
+    attr_reader :word
+
+    def initialize(word)
+      @word = word
+    end
+
+    def pronunciations
+      north, south = ipa.zip(approximations)
+      { north: north, south: south }
+    end
+
+    def approximations
+      ['', '']
+    end
+
+    def ipa
+      north_long_i = stress_i == syllables.length - 1 ? stress_i : nil
+      north_syllables = long_syllables(stressed_syllables, north_long_i).map do |syll|
+        syll.sub(/(?<=#{Vowel})(?=(ɬ|s)#{Consonant})/, 'ː') # long vowels before consonant clusters beginning ll and s
+      end
+      north_ipa = deprotect_vowels(north_syllables.join)
+        .gsub('l', 'ɫ') # dark l
+
+      south_syllables = long_syllables(stressed_syllables, stress_i).each_with_index.map do |syll, i|
+        if i == syllables.length - 2 then syll.sub(/(?<=#{Vowel})ː(?=ɬ#{Consonant})/, '') # short vowels in the penult before ll
+        else syll
+        end
+      end
+      south_ipa = deprotect_vowels(south_syllables.join)
+        .gsub('ɨ̞', 'ɪ') # u -> i
+        .gsub('ɨ', 'i')
+        .gsub('ɑːi', 'ai')
+
+      # TODO acute accent
+      # TODO short vowel w
+      # TODO irregular stress
+      # TODO epenthetic echo vowel, eg. cenedl -> 'kenedel
+
+      [north_ipa, south_ipa]
+    end
+
+    def syllables
+      @syllables ||=
+        base_ipa.to_enum(:scan, /^#{Consonant}*|(?<!#{UncoveredConsonant})#{Consonant}#{Vowel}|(?<=#{UncoveredConsonant})#{Consonant}+#{Vowel}/).map { Regexp.last_match.begin(0) } # find starting index of each syllable
+        .reverse.scan(base_ipa.length..0) { |prev, pos| pos...prev.first }.reverse # map the indices to ranges
+        .map { |range| base_ipa[range] } # map indices to substrings (ie. syllables)
+    end
+
+    def base_ipa
+      @base_ipa ||= word \
+        .gsub(/[^[[:word:]]]/, '') \
+        # protect vowels from conflict with ipa vowels
+        .gsub('a', '@')
+        .gsub('e', '%')
+        .gsub('i', '!')
+        .gsub('o', '&')
+        .gsub('u', '#') \
+        # consonants
+        .gsub(/c(?!h)/, 'k')
+        .gsub('dd', 'ð')
+        .gsub(/ff|ph/, 'f')
+        .gsub('j', 'dʒ')
+        .gsub('ll', 'ɬ')
+        .gsub('mh', 'm̥')
+        .gsub('nh', 'n̥')
+        .gsub('ngh', 'ŋ̊')
+        .gsub('rh', 'r̥')
+        .gsub(/s!(?=#{Vowel})|sh|(?<=t)ch$/, 'ʃ')
+        .gsub('th', 'θ')
+        .gsub('ch', 'x')
+        .gsub(/(?<!f)f/, 'v')
+        .gsub(/!(?=#{Vowel})/, 'j') \
+        # w glides
+        .gsub(/(?<=g)w(?=r|n)/, 'ʷ')
+        .gsub(/(?<=ch)w/, 'ʷ') \
+        # long vowels
+        .gsub('â', 'ɑː')
+        .gsub('ê', 'eː')
+        .gsub('î', 'iː')
+        .gsub('ô', 'oː')
+        .gsub(/û|ŷ/, 'ɨː')
+        .gsub('ŵ', 'uː') \
+        # short vowels
+        .gsub('à', 'a')
+        .gsub('è', 'ɛ')
+        .gsub('ì', 'ɪ')
+        .gsub('ò', 'ɔ')
+        .gsub(/ù|ỳ/, 'ɨ̞')
+        .gsub('ẁ', 'ʊ') \
+        # diphthongs TODO how many of these are needed
+        .gsub(/@#|á#/, 'aɨ')
+        .gsub('@w', 'au') # TODO also long a in north
+        .gsub('@%', 'ɑːɨ')
+        .gsub('%!', 'əi')
+        .gsub('%#', 'əɨ')
+        .gsub('%w', 'ɛu') # TODO also long e in north
+        .gsub('!w', 'ɪu')
+        .gsub(/#w|yw/, 'ɨu') # TODO also əu for yw sometimes
+        .gsub('&!', 'ɔi')
+        .gsub('&#', 'ɔɨ') # TODO also long o in north
+        .gsub('&%', 'ɔɨ') # TODO also long o in north
+        .gsub(/(?<!#{Vowel})wy/, 'ʊɨ') # TODO also long w in north. sometimes w is w, not u, depending on preceeding consonant
+        .gsub('y', 'ə') # lucky last. undo this in the final syllable later on
+    end
+
+    def stress_i
+      @stress_i ||= syllables.length == 1 ? 0 : syllables.length - 2
+    end
+
+    def stressed_syllables
+      # TODO acute accent (´) is sometimes used to mark a stressed final syllable in a polysyllabic word.
+      @stressed_syllables ||= syllables.each_with_index.map do |syll, i|
+        stressed = i == stress_i ? 'ˈ' + syll : syll
+        if i == syllables.length - 1 then stressed.sub(/(?<=#{UncoveredConsonant})ə(?=#{UncoveredConsonant})#{syllables.length > 1 ? '?' : ''}/, '#')
+        else stressed
+        end
+      end
+    end
+
+    def long_syllables(syllables, long_index)
+      syllables.each_with_index.map do |syll, i|
+        if i == long_index then long_syllable(syll, syllables[i + 1])
+        else syll
+        end
+      end
+    end
+
+    def long_syllable(syllable, next_syll)
+      if next_syll.nil?
+        if syllable !~ /#{Consonant}/ then syllable # this should only match single-vowel words
+        else
+          syllable
+            .sub(/(?<!#{Vowel})(?<=#{LongableVowel})(?=[bdðfgθvx]$)/, 'ː') # preceeding certain consonants
+            .sub(/(?<=#{LongableVowel})(?=s?$)/, 'ː') # word-final s or word-final vowel
+        end
+      elsif syllable =~ /#{LongableVowel}$/ and next_syll =~ /^[bdðfgθvx]/ then syllable + 'ː' # preceeding certain consonants
+      else syllable
+      end
+    end
+
+    def deprotect_vowels(word)
+      word
+        .gsub('@ː', 'ɑː')
+        .gsub('@', 'a')
+        .gsub('%ː', 'eː')
+        .gsub('%', 'ɛ')
+        .gsub('!ː', 'iː')
+        .gsub('!', 'ɪ')
+        .gsub('&ː', 'oː')
+        .gsub('&', 'ɔ')
+        .gsub('#ː', 'ɨː')
+        .gsub('#', 'ɨ̞')
+    end
+  end
+end
